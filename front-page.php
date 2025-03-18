be5ba0263d0cdede8b6adcae326d2db1dd708732
HIRAYAMA\reona <belinklab@gmail.com>
2025-03-19 00:03:19 +0900

    CTA、ヘッダー、FV作成 Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	assets/scss/common/_common.scss

--- front-page.php
+++ front-page.php
@@ -8,6 +8,8 @@ Template Name: トップページ
 <?php get_template_part('template-parts/header'); ?>
 
 <main>    
+    <?php get_template_part('template-parts/fv'); ?>
+    <?php get_template_part('template-parts/cta'); ?>
     <?php get_template_part('template-parts/price'); ?>
     <?php get_template_part('template-parts/faq'); ?>
     <?php get_template_part('template-parts/company'); ?>

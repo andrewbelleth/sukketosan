1b35a2634e65f69b79951af5ea52392b52192d4c
HIRAYAMA\reona <belinklab@gmail.com>
2025-03-18 15:14:41 +0900

    ファイル整理

--- template-parts/contact.php
+++
@@ -1,9 +0,0 @@
-<section class="contact" id="contact">
-    <div class="wrapper fadeIn">
-        <h2 class="contact__title c-title c-title__white">お問い合わせ</h2>
-
-        <div class="contact__container">
-            <?php echo do_shortcode('[contact-form-7 id="72977d3" title="コンタクトフォーム 1"]'); ?>
-        </div>
-    </div>
-</section>

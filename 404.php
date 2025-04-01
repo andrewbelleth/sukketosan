808e4e6aec1514687ff136cc1ef142697f9204c1
andrewbelleth <andrewbelleth@gmail.com>
2025-04-01 17:56:19 +0900

    404追加

--- 404.php
+++ 404.php
@@ -7,10 +7,10 @@ if (! defined('ABSPATH')) exit;
 <main>
     <div class="container">
         <div class="page">
-            <div class="page__body wrapper">
-                <h2 class="404__title">お探しのページが見つかりませんでした。</h2>
-                <p class="404__text">お探しのページが見つかりませんでした。</p>
-                <a href="<?php echo home_url("/sukettosan"); ?>" class="404__link">トップページに戻る</a>
+            <div class="page__body wrapper page-404">
+                <h2 class="page-404__title">お探しのページが見つかりませんでした。</h2>
+                <p class="page-404__text">お客様がお探しのページは移動、または、削除された可能性があります。<br>お手数ですが、以下より必要な情報をお探しください。</p>
+                <a href="<?php echo home_url("/"); ?>" class="page-404__link">トップページに戻る</a>
             </div>
 
         </div>
